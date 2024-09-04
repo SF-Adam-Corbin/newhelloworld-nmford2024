79169bf9139cd0733cc51d08a1c6b7892b50250d
nmford2024 <nate.ford@go.sfcollege.edu>
2024-09-03 22:23:03 -0400

    Add files via upload

--- mine (4).py
+++ mine (4).py
@@ -1,10 +1,10 @@
 # Write a line of code that prints "Hello world!".
-print("Hello World")
+print("Hello world!")
 
 
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
-  message = "Hello world"
+  message = "Hello world!"
   print(message)
   
   return variable_name
